ee0ed3cb609310d8726fb739676fa498b71bc959
Kelly Bazilio <47607404+KellyBazilio@users.noreply.github.com>
2024-12-03 04:01:21 -0300

    teste

---
+++ helpdesk.js
@@ -0,0 +1,53 @@
+var config = {
+    host: 'prevendasin.us.qlikcloud.com',
+    prefix: '/',
+    port: 443,
+    isSecure: true,
+    webIntegrationId: 'SI31dmGzwFBmA3QkKHAumN6eoIlnAssf'
+};
+
+//Redirect to login if user is not logged in
+async function login() {
+      function isLoggedIn() {
+        return fetch("https://"+config.host+"/api/v1/users/me", {
+          method: 'GET',
+          mode: 'cors',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+            'qlik-web-integration-id': config.webIntegrationId,
+          },
+        }).then(response => {
+          return response.status === 200;
+        });
+      }
+      return isLoggedIn().then(loggedIn => {
+        if (!loggedIn) {	  
+            window.location.href = "https://"+config.host+"/login?qlik-web-integration-id=" + config.webIntegrationId + "&returnto=" + location.href;
+            throw new Error('not logged in');
+        }
+      });
+    }
+login().then(() => {
+    require.config( {
+    baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources",
+    webIntegrationId: config.webIntegrationId
+} );			
+
+require( ["js/qlik"], function ( qlik ) {
+	qlik.on( "error", function ( error ) {
+		$( '#popupText' ).append( error.message + "<br>" );
+		$( '#popup' ).fadeIn( 1000 );
+	} );
+	$( "#closePopup" ).click( function () {
+		$( '#popup' ).hide();
+	} );
+    //open apps -- inserted here --
+	var app = qlik.openApp( 'c5811972-cc26-4da4-aac2-bb8d59935517', config );
+	
+    //get objects -- inserted here --
+	app.visualization.get('YZgRC').then(function(vis){
+    vis.show("QV01");	
+	} );
+    
+} );});
